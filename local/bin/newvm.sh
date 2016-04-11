5405ab12b7742d6933fe3d4dee55b20078d0421f
Tom Sorensen <tsorensen@gmail.com>
2016-04-10 22:12:39 -0400

    change to virt-clone/customize, add Ansible bits

--- local/bin/newvm.sh
+++ local/bin/newvm.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-IMGDIR="/srv/vm/images/"
-XMLDIR="/srv/vm/xml/"
+IMGDIR="/srv/vm/images"
+XMLDIR="/srv/vm/xml"
 
 # $1 = new name
 newvm="$1"
 
-# copy template image -- this already has the key injected or root pw set
-cp "$IMGDIR/c7-key.qcow2" "$IMGDIR/$newvm.qcow2"
+# copy template image -- this already has the keys injected; root pw is
+# disabled:
+virt-clone -o c7-template -n "${newvm}" --auto-clone
 
-# run sed on xml
-sed -e "s/VMNAME/$newvm/" "$XMLDIR/c7-base.xml" > "$XMLDIR/$newvm.xml"
-# may need to gen UUID
-# -e "s/UUID/$(uuidgen)/"
-# may need to change mac
-# -e "s/MAC/$(macgen.py)/"
+# use desc field for ansible groups
+virsh desc "${newvm}" '{"groups": ["minecraft"]}'
+
+# set the hostname; makes things easier to do now
+virt-customize --hostname "${newvm}" -d "${newvm}"
 
 # define VM
-sudo virsh define "$XMLDIR/$newvm.xml"
+virsh start "${newvm}"
